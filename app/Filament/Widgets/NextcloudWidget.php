f3dddf6d4068d3d3f1f09bcfb66d2953310ceb9a
Omar Valdez <valdez-omar@outlook.com>
2025-09-19 12:34:42 -0400

    Bug: Se mejoro el widget nextcloud para que reconozca el dispositivo y redireccione segun dispositivo

--- app/Filament/Widgets/NextcloudWidget.php
+++ app/Filament/Widgets/NextcloudWidget.php
@@ -4,28 +4,43 @@ namespace App\Filament\Widgets;
 
 use Filament\Widgets\Widget;
 use BezhanSalleh\FilamentShield\Traits\HasWidgetShield;
+use Detection\MobileDetect; // importamos Mobile Detect
 
 class NextcloudWidget extends Widget 
 {
+    use HasWidgetShield;
+
     protected static string $view = 'filament.widgets.nextcloud-widget';
-    protected static ?string $heading = 'Nextcloud widget';
-    use HasWidgetShield;    
+    protected static ?string $heading = 'Archivos Compartidos';
 
     public function getNextcloudUrl(): string
     {
         return 'https://nube.ailem.app';
     }
+    
+    /**
+     * Determina qué enlace de descarga mostrar según dispositivo
+     */
+    public function getDownloadLink(): string
+    {
+        $detect = new MobileDetect();
+
+        if ($detect->isiOS()) {
+            // iPhone / iPad
+            return 'https://apps.apple.com/es/app/nextcloud/id1125420102';  
+        } elseif ($detect->isAndroidOS()) {
+            return 'https://play.google.com/store/apps/details?id=com.nextcloud.client';  // enlace de Google Play
+        } else {
+            // por defecto o Windows / escritorio
+            return 'https://github.com/nextcloud-releases/desktop/releases/download/v3.17.2/Nextcloud-3.17.2-x64.msi';
+        }
+    }
 
     public function getClientDownloadUrl(): string
     {
-        return 'https://github.com/nextcloud-releases/desktop/releases/download/v3.16.4/Nextcloud-3.16.4-x64.msi';
+        // Si quieres, puedes combinar con getDownloadLink:
+        return $this->getDownloadLink();
     }
-
-    // // Esto evita que Filament busque rutas
-    // public static function canView(): bool
-    // {
-    //     return true;
-    // }
 
     /**
      * @return int|string|array<string, int|null>
