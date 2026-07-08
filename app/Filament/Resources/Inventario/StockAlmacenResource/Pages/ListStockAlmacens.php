5fabc3e2c2e1aac6020cc5c9c4a6e0a62e63fa02
Omar Valdez <valdez-omar@outlook.com>
2026-07-08 15:05:37 -0400

    Funcionalidad: Se creo el resource para administrar el stock de almacenes

---
+++ app/Filament/Resources/Inventario/StockAlmacenResource/Pages/ListStockAlmacens.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Filament\Resources\Inventario\StockAlmacenResource\Pages;
+
+use App\Filament\Resources\Inventario\StockAlmacenResource;
+
+use Filament\Resources\Pages\ListRecords;
+
+class ListStockAlmacens extends ListRecords
+{
+    protected static string $resource = StockAlmacenResource::class;
+    
+}
